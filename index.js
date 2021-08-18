52b41ea1b2c10d34fa4b20f1e7c2ee1d9a0209be
Mariusz <mar.lewandowski@gmail.com>
2021-08-18 19:09:26 +0200

    'stage2w1d3'

---
+++ index.js
@@ -0,0 +1,30 @@
+// 3# sposób
+// const dns = require('dns').promises;
+// (async ()=>{
+//     const data = await  dns.lookup('google.com');
+//     console.log(data.address);
+// })()
+// wyszło216.58.215.78
+
+
+// 2# sposób
+// const dns = require('dns');
+// const {promisify} = require('util');
+//
+// const dnsPromised = promisify(dns.lookup)
+//
+// dnsPromised('google.com').then(data => console.log(data.address)).catch(err => console.log('aaa',err))
+// wyszło 216.58.215.78
+
+
+// 1# sposób
+// const dns = require('dns');
+//
+// dns.lookup('google.com', (err, data)=> {
+//     if(err){
+//         console.log('aaaa', err)
+//     }    else {
+//         console.log(data);
+//     }
+// })
+// wyszło 216.58.215.78
