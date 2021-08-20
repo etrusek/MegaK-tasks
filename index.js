8c091b408ec92417f95fc9f8c99b8f37956d3e1d
Mariusz <mar.lewandowski@gmail.com>
2021-08-20 09:06:28 +0200

    s2w1d5

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-// tworzenie i zapisywanie plików
-const {writeFile, readFile, appendFile} = require('fs').promises;
+// sprawdzanie czy istnieje
+const {access} = require('fs').promises;
+const {W_OK} = require('fs').constants;
+const FILE_NAME = './src/basket.js';
 
-
-(async ()=>{
-    try {
-        const data = await readFile('./data/hello.txt', 'utf8')
-        const lastValue = Number(data.split('\n')[data.split('\n').length - 1]);
-        console.log(lastValue)
-        await writeFile('./data/hello.txt', `\n${lastValue * 2}`, {flag: 'a'});
-    } catch (e) {
-        console.log('aaaa', e)
-    }
+(async () => {
+    const fileExist = await access(FILE_NAME, W_OK)
+    console.log(fileExist) // NIEDZIAla bo ma niedziałać. Nie sprawdzać czy plik istnieje. Bo po pewnym czasie przy próbie odczytu lub zapisu może już nie istnieć.
 })()
 
+
+// odczytywanie statystyk plików
+// const {stat, readdir} = require('fs').promises;
+// (async () => {
+//     try {
+//         const files = await readdir('./src');
+//         for (const file of files) {
+//             const fileStat = await stat(`./src/${file}`)
+//             console.log(fileStat);
+//         }
+//     } catch (err) {
+//         console.error(err);
+//     }
+// })()
+
+// odczytywanie zawartości folderów
+// const {readdir} = require('fs').promises;
+// (async () => {
+//     try {
+//         const files = await readdir('./src', {withFileTypes : true});
+//         for (const file of files) {
+//             console.log(file);
+//         }
+//
+//     } catch (err) {
+//         console.error(err);
+//     }
+// })()
+
+
+// tworzenie i zapisywanie plików
+// const {writeFile, readFile, appendFile} = require('fs').promises;
+//
+//
+// (async ()=>{
+//     try {
+//         const data = await readFile('./data/hello.txt', 'utf8')
+//         const lastValue = Number(data.split('\n')[data.split('\n').length - 1]);
+//         console.log(lastValue)
+//         await writeFile('./data/hello.txt', `\n${lastValue * 2}`, {flag: 'a'});
+//     } catch (e) {
+//         console.log('aaaa', e)
+//     }
+// })()
 
 
 // 3# sposób
